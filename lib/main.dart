117487dcd60ea9fc773dac02af4242b66d2c2643
Ilja Semjonov <ilsemjonov@gmail.com>
2020-05-21 21:34:54 +0300

    scaffold widget with app bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
-// Import a helper library
-// from flutter to get content on the screen
 import 'package:flutter/material.dart';
 
-// Define a 'main' function to run when out app starts
 void main() {
-  // Create a new text widget to show some text on the screen
   var app = MaterialApp(
-    home: Text('Hi there!'),
+    home: Scaffold(
+      appBar: AppBar(),
+    ),
   );
 
-  // Take that widget and get it on the screen
   runApp(app);
 }
