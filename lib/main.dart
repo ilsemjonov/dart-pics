7499fae301a18ccbe4152c1511bcaed20425b292
Ilja Semjonov <ilsemjonov@gmail.com>
2020-06-06 15:40:28 +0300

    custom app widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
+import 'src/app.dart';
 
 void main() {
-  var app = MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Images'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print('Yo!');
-        },
-        child: Icon(Icons.add),
-      ),
-    ),
-  );
-
-  runApp(app);
+  runApp(App());
 }
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,1 +1,19 @@
 import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Images'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('Yo!');
+          },
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
