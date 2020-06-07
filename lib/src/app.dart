11cb38691471a3ab42a395594e4fe882334fa95d
Ilja Semjonov <ilsemjonov@gmail.com>
2020-06-07 15:14:05 +0300

    http import

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
 
 class App extends StatefulWidget {
   createState() {
@@ -9,16 +10,17 @@ class App extends StatefulWidget {
 class AppState extends State<App> {
   int counter = 0;
 
+  void fetchImage() {
+    counter++;
+    get('http://jsonplaceholder.typicode.com/photos/$counter');
+  }
+
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
         body: Text('$counter'),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              counter += 1;
-            });
-          },
+          onPressed: fetchImage,
           child: Icon(Icons.add),
         ),
         appBar: AppBar(
